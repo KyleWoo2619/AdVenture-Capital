09311c8b694c977259316dcdfb6ca79d4a1d5b5c
da413 <danielwolfcol@gmail.com>
2025-09-09 19:18:09 -0400

    platform spawner script, changed scene

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -1,20 +1,38 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    /* while the game is going 
-     * add y to the platform spawner position, then add x to the platform spawner position
-     * spawn a new platform
-     * loop this until player loses
-     */
+    public Transform groundPlatform;
+   [SerializeField] float hValue;
+    [SerializeField] float vValue;
 
+    private float spawnerPosX;
+    private float spawnerPosY;
 
+    private int RandNum;
     
     void Start()
     {
+        hValue = 5; vValue = 1;
+        spawnerPosX = groundPlatform.transform.position.x + hValue; spawnerPosY = groundPlatform.transform.position.y + vValue;
+        RandNum = Random.Range(0, 5);
+        Debug.Log(RandNum);
+
+        if (RandNum < 3)
+        {
+            spawnerPosX = spawnerPosX * 1;
+        }
+        else
+        {
+            spawnerPosX = -1*spawnerPosX;
+        }
+        
+        transform.position = new Vector3(spawnerPosX, spawnerPosY, groundPlatform.transform.position.z); //sets initial position of spawner
+        
         if (GameManager.instance.gameIsPlaying)
         {
-            InvokeRepeating("MoveSpawner", 1, 5);
+           InvokeRepeating("MoveSpawner", 2, 5);
         }
     }
 
@@ -26,13 +44,25 @@ public class PlatformSpawner : MonoBehaviour
         
         
 
-        // transform.position = transform.position + new Vector3(-5, transform.position.y, 0);
+        
     }
 
     void MoveSpawner()
     {
-        transform.position = transform.position + Vector3.up;
+
+        spawnerPosY += 1;
+        spawnerPosX = -(spawnerPosX); //sets spawnerPosX to negative of itself to move to otherside
+        transform.position = new Vector3(spawnerPosX, spawnerPosY, groundPlatform.transform.position.z); //moves the transform to the otherside
     }
 
-    
+    void SpawnLeftMovingPlatform()
+    {
+        
+    }
+
+    void SpawnRightMovingPlatform()
+    {
+        
+    }
+
 }
