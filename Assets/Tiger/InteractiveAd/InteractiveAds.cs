3e4bb2ebc88df91bb41bed569d894edc5414f463
Tiger <135745508+tabritt@users.noreply.github.com>
2025-10-15 17:42:04 -0400

    Workable state. need to fix up the text on buttons

--- Assets/Tiger/InteractiveAd/InteractiveAds.cs
+++ Assets/Tiger/InteractiveAd/InteractiveAds.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InteractiveAds : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class InteractiveAds : MonoBehaviour
         public Sprite image;
         public string choiceAText;
         public string choiceBText;
-        public int nextStepA = -1; // -1 ends the ad
+        public int nextStepA = -1; // -1 means this choice ends the ad
         public int nextStepB = -1;
     }
 
@@ -20,8 +21,8 @@ public class InteractiveAds : MonoBehaviour
     [SerializeField] private Image adImage;
     [SerializeField] private Button choiceAButton;
     [SerializeField] private Button choiceBButton;
-    [SerializeField] private Text choiceALabel;
-    [SerializeField] private Text choiceBLabel;
+    [SerializeField] private TextMeshProUGUI choiceALabel;
+    [SerializeField] private TextMeshProUGUI choiceBLabel;
 
     [Header("Ad Steps")]
     [SerializeField] private List<AdStep> adSteps = new();
@@ -35,11 +36,13 @@ public class InteractiveAds : MonoBehaviour
 
         currentStepIndex = 0;
         onComplete = onAdComplete;
+
         ShowCurrentStep();
     }
 
     private void ShowCurrentStep()
     {
+        // If the step index is invalid, just end the ad
         if (currentStepIndex < 0 || currentStepIndex >= adSteps.Count)
         {
             Debug.LogWarning("[InteractiveAd] Invalid step index: " + currentStepIndex);
@@ -51,17 +54,19 @@ public class InteractiveAds : MonoBehaviour
 
         Debug.Log($"[InteractiveAd] Showing step: {step.stepName} (Index {currentStepIndex})");
 
+        // Set image and button texts
         if (adImage) adImage.sprite = step.image;
-
         if (choiceALabel) choiceALabel.text = step.choiceAText;
         if (choiceBLabel) choiceBLabel.text = step.choiceBText;
 
+        // Set up Choice A button
         if (choiceAButton)
         {
             choiceAButton.onClick.RemoveAllListeners();
             choiceAButton.onClick.AddListener(() => MakeChoice(step.nextStepA, "A"));
         }
 
+        // Set up Choice B button
         if (choiceBButton)
         {
             choiceBButton.onClick.RemoveAllListeners();
@@ -75,7 +80,7 @@ public class InteractiveAds : MonoBehaviour
 
         if (nextStep == -1)
         {
-            Debug.Log("[InteractiveAd] Reached end of path. Finishing ad.");
+            Debug.Log("[InteractiveAd] no index after this choice, Closing !.");
             EndAd();
         }
         else
